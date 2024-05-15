ac4805d68df18fc06cc189866adae224e727e672
Ario <ariobarxan.dev@gmail.com>
2024-05-15 17:46:25 +1000

    Initial Commit

---
+++ MarkdownNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarkdownNote
+//
+//  Created by Home on 15/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarkdownNote/MarkdownNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownNoteApp.swift
+//  MarkdownNote
+//
+//  Created by Home on 15/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
